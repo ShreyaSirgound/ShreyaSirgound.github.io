ec227457cd6514e9c1bac8ce1fd8668abdda16fb
ShreyaSirgound <shreya.sirgound@student.tdsb.on.ca>
2024-07-30 13:58:40 -0400

    added favicon + socials transition

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function () {
     'use strict';
     var view = $(window).height();
     $('.part').height(view).scrollie({
-        scrolloffset: -100,
+        scrolloffset: -50,
         scrollingInView: function (elem) {
             var bgColor = elem.data('background')
             $('body').css('background-color', bgColor);
