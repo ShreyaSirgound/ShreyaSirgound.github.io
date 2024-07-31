6bb3b87c1b52f0021e3c36c1ee31f261ebc6ede7
ShreyaSirgound <shreya.sirgound@student.tdsb.on.ca>
2024-07-31 14:49:11 -0400

    updated phone responsivity

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function () {
     'use strict';
     var view = $(window).height();
     $('.part').height(view).scrollie({
-        scrolloffset: -500,
+        scrolloffset: -50,
         scrollingInView: function (elem) {
             var bgColor = elem.data('background')
             $('body').css('background-color', bgColor);
